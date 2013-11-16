6982efe7e87d105301271e61a3869afc4bd40bcb
alvaromuir <alvaromuir@gmail.com>
2013-11-16 12:19:56 -0500

    Generated the Rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chitlist::Application.config.secret_key_base = 'c25e26b86089b7f20723e9f7f7a7db1ae99fa53f0d6aad31c26328ac06f0dd868df6807883063feac7cac26ae7b860007873f3a8dc1739df0e13efacb55f3011'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chitlist::Application.config.session_store :cookie_store, key: '_chitlist_session'
