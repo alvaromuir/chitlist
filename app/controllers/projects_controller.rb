fd5552fcaa3912d837b2cccdda8ba37e7cbc1a45
alvaromuir <alvaromuir@gmail.com>
2013-11-19 10:04:44 -0500

    Add validation ot ensure names are specified when creating projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -12,6 +12,10 @@ class ProjectsController < ApplicationController
     if @project.save
       flash[:notice] = "Project has been created."
       redirect_to @project
+    else
+      flash[:alert] = "Project has not been created."
+
+      render "new"
     end
   end
 
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -2,11 +2,13 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
-  scenario "can create a project" do
+  before do
     visit '/'
     
     click_link 'New Project'
-    
+  end
+
+  scenario "can create a project" do
     fill_in 'Name', with: 'Work Project'
     fill_in 'Description', with: 'Some project for work'
     click_button 'Create Project'
@@ -21,4 +23,12 @@ feature 'Creating Projects' do
     expect(page).to have_title(title)
 
   end
+
+  scenario "can not create a project without a name" do
+    click_button 'Create Project'
+
+    expect(page).to have_content("Project has not been created.")
+    expect(page).to have_content("Name can't be blank")
+  end
+
 end
