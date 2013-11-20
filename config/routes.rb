35e675f5aa4b8e62b7a67727391b53c952efbb52
Alvaro Muir <alvaromuir@gmail.com>
2013-11-20 01:46:22 -0500

    Implemented signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Chitlist::Application.routes.draw do
   root "projects#index"
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
   
   resources :projects do
     resources :tasks
